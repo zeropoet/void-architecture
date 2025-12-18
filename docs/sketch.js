0f403840d5f11b6e27c427871e12a42c3a2b0068
Zeropoet <mancel@zeropoet.xyz>
2025-12-18 14:32:59 -0500

    ovel ignition: success

--- docs/sketch.js
+++ docs/sketch.js
@@ -1,13 +1,26 @@
 function setup() {
-    createCanvas(100, 100);
+    createCanvas(1454, 705);
 }
 function draw() {
     background(225);
+
+    let mouse = createVector(mouseX, mouseY);
+    let center = createVector(width / 2, height / 2);
+    stroke(200);
+    strokeWeight(1);
+    line(0, 0, center.x, center.y);
+    line(0,0, mouse.x, mouse.y)
+    mouse.sub(center);
+    stroke(2);
+    translate(width / 2, height / 2);
+    line(0, 0, mouse.x, mouse.y);
+
     noStroke();
     if (mouseIsPressed) {
         fill(0);
     } else {
         fill(255);
     }
-    rect(40, 0,20,20);
+    rect(-400, -233,20,-20);
+    rect(1, 0,5,-5);
 }
