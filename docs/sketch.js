b07b6065a13cb81a3a056198651bd6d607f2cd4a
Zeropoet <mancel@zeropoet.xyz>
2025-12-18 20:13:43 -0500

    ovel-5

--- docs/sketch.js
+++ docs/sketch.js
@@ -6,11 +6,17 @@ function draw() {
 
     let mouse = createVector(mouseX, mouseY);
     let center = createVector(width / 2, height / 2);
+    mouse.sub(center);
+    mouse.mult(0.8);
+    let m = mouse.mag();
+    fill(242);
+    rect(0,0, m/5,10);
+
     stroke(200);
     strokeWeight(1);
-    line(0, 0, center.x, center.y);
-    line(0,0, mouse.x, mouse.y)
-    mouse.sub(center);
+    line(0, 50, center.x, center.y);
+    rect(1, 0,m-2,-1);
+
     stroke(2);
     translate(width / 2, height / 2);
     line(0, 0, mouse.x, mouse.y);
@@ -21,6 +27,7 @@ function draw() {
     } else {
         fill(255);
     }
-    rect(-400, -233,20,-20);
-    rect(1, 0,5,-5);
+    rect(-400/m, -233,10,-10);
+    rect(1, 0,m-2,-1);
+
 }
