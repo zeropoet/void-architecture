a9d546162db46d5941962a4d4251c29a51679fb4
Zeropoet <mancel@zeropoet.xyz>
2025-12-19 00:33:45 -0500

    ovel-6

--- docs/sketch.js
+++ docs/sketch.js
@@ -1,14 +1,23 @@
+let mover;
+let m;
+
 function setup() {
     createCanvas(1454, 705);
+    mover = new Mover();
 }
+
 function draw() {
     background(225);
+
+    mover.update();
+    mover.checkEdges();
+    mover.show();
 
     let mouse = createVector(mouseX, mouseY);
     let center = createVector(width / 2, height / 2);
     mouse.sub(center);
     mouse.mult(0.8);
-    let m = mouse.mag();
+    m = mouse.mag();
     fill(242);
     rect(0,0, m/5,10);
 
@@ -22,12 +31,44 @@ function draw() {
     line(0, 0, mouse.x, mouse.y);
 
     noStroke();
-    if (mouseIsPressed) {
+    if (mouseIsPressed) {m
         fill(0);
     } else {
         fill(255);
     }
     rect(-400/m, -233,10,-10);
     rect(1, 0,m-2,-1);
+}
 
+class Mover {
+    constructor() {
+        this.position = createVector(700, 150);
+        this.velocity = createVector(random(0,1), random(0,0));
+    }
+    update() {
+        this.position.add(this.velocity);
+        this.acceleration = p5.Vector.random2D();
+        this.velocity.add(this.acceleration);
+        this.velocity.limit(.15);
+        this.position.add(this.velocity);
+    }
+    show() {
+        stroke(0);
+        strokeWeight(0);
+        fill(0,0,0);
+        rect(this.position.x, this.position.y, 5, 5);
+    }
+    checkEdges() {
+        if (this. position.x >width) {
+            this.position.x = 0;
+        } else if (this.position.x < 0) {
+            this.position.x = width;
+        }
+
+        if (this.position.y > height ) {
+            this.position.y = 0;
+        } else if (this.position.y < 0) {
+            this.position.y = height;
+        }
+    }
 }
